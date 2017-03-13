088ad3d8768eaf1546f659e48a0251c125b79782
Macko451 <Macko451@gmail.com>
2017-03-13 19:57:01 +0100

    Two projetcs in two folders
    Added another mini-game for my project

---
+++ Nauka Telekinezy/Assets/Scripts/PickUpRotation.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickUpRotation : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3 (0.0f, 30, 0.0f) * Time.deltaTime);
+	}
+}
---
+++ Uczen Czarodzieja/Assets/Uczeń Czarodzieja/Scripts/LevelManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour 
+{
+	public void LoadLeve(string name) 
+	{
+		SceneManager.LoadScene(name);
+	}
+	
+}
