4551dc3e9d6900dd0019ebfea20028aaaab6c6e3
Macko451 <Macko451@gmail.com>
2017-04-06 17:46:00 +0200

    First commit

---
+++ Magiczna Obrona/Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class WaveSpawner : MonoBehaviour {
+
+	public Transform enemy;
+	public Transform spawnPoint;
+	public Text waveCountdownText;
+
+	public float timeBetweenWaves;
+
+	private float countdown = 2f;
+	private int waveIndex = 0;
+
+	void Start () 
+	{
+		CountdownUpdate();
+	}
+
+	void Update () 
+	{
+		if (countdown <= 0f) 
+		{
+			StartCoroutine (SpawnWave());
+			countdown = timeBetweenWaves;
+		}
+
+		countdown -= Time.deltaTime;
+		CountdownUpdate();
+	}
+
+	IEnumerator SpawnWave ()
+	{
+		waveIndex++;
+
+		for (int i = 0; i < waveIndex; i++) 
+		{
+			SpawnEnemy();
+			yield return new WaitForSeconds (0.5f);
+		}
+	}
+
+	public void SpawnEnemy () 
+	{
+		Instantiate (enemy, spawnPoint.position, spawnPoint.rotation);
+	}
+
+	public void CountdownUpdate () 
+	{
+		waveCountdownText.text = "Countdown: " + Mathf.Round(countdown).ToString();
+	}
+}
